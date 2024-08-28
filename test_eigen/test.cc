88006b97ef4c8841e8ce7580c13ec9f654f3e2ad
TianHaijian <tianhaijian@immotors.com>
2024-08-28 23:50:46 +0800

    opt func

--- test_eigen/test.cc
+++ test_eigen/test.cc
@@ -5,79 +5,35 @@
 #include <string>
 #include <stdexcept>
 
-int main() {
-    // 定义60x60的Eigen矩阵
-    Eigen::MatrixXd A(60, 60);
-    Eigen::MatrixXd B(60, 2);
-    Eigen::MatrixXd C(60, 2);
-    Eigen::MatrixXd T(60, 6);
-    // 打开txt文件
-    std::ifstream file("df_A_martrix.txt");
+void feedMartrix(Eigen::MatrixXd& A, const std::string file_name){
+    std::ifstream file(file_name);
     std::string line;
     int row = 0;
     // 逐行读取文件内容
-    while (std::getline(file, line) && row < 60) {
+    while (std::getline(file, line) && row < A.rows()) {
         std::istringstream ss(line);
         double value;
         int col = 0;
         // 逐个读取值，自动跳过多余空格
-        while (ss >> value && col < 60) {
+        while (ss >> value && col < A.cols()) {
             A(row, col) = value;
             col++;
         }
         row++;
     }
     file.close();
+}
+int main() {
+    // 定义60x60的Eigen矩阵
+    Eigen::MatrixXd A(60, 60);
+    Eigen::MatrixXd B(60, 2);
+    Eigen::MatrixXd C(60, 2);
+    Eigen::MatrixXd T(60, 6);
 
-    // 打开txt文件
-    std::ifstream file2("df_b_martrix.txt");
-    row = 0;
-    // 逐行读取文件内容
-    while (std::getline(file2, line) && row < 60) {
-        std::istringstream ss(line);
-        double value;
-        int col = 0;
-        // 逐个读取值，自动跳过多余空格
-        while (ss >> value && col < 2) {
-            B(row, col) = value;
-            col++;
-        }
-        row++;
-    }
-    file2.close();
-    // 打开txt文件
-    std::ifstream file3("df_c_martrix.txt");
-    row = 0;
-    // 逐行读取文件内容
-    while (std::getline(file3, line) && row < 60) {
-        std::istringstream ss(line);
-        double value;
-        int col = 0;
-        // 逐个读取值，自动跳过多余空格
-        while (ss >> value && col < 2) {
-            C(row, col) = value;
-            col++;
-        }
-        row++;
-    }
-    file3.close();
-
-    std::ifstream file4("df_t_vec.txt");
-    row = 0;
-    // 逐行读取文件内容
-    while (std::getline(file4, line) && row < 60) {
-        std::istringstream ss(line);
-        double value;
-        int col = 0;
-        // 逐个读取值，自动跳过多余空格
-        while (ss >> value && col < 6) {
-            T(row, col) = value;
-            col++;
-        }
-        row++;
-    }
-    file4.close();
-
+    feedMartrix(A, "df_A_martrix.txt");
+    feedMartrix(B, "df_b_martrix.txt");
+    feedMartrix(C, "df_c_martrix.txt");
+    feedMartrix(T, "df_t_vec.txt");
     double result = C.block<6, 1>(6, 0).dot(T.block<6, 1>(0, 0).transpose());
     std::cout << "result: " << result << std::endl;
     return 0;
